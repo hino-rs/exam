0e94701711853227691d54770b8b158ccf232314
kaito hino <n041969944@gmail.com>
2026-04-16 12:28:49 +0900

    feat: schoolDao

--- src/main/java/dao/SchoolDao.java
+++ src/main/java/dao/SchoolDao.java
@@ -1,3 +1,25 @@
 package dao;
 
-public class SchoolDao
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import bean.School;
+
+public class SchoolDao extends DAO {
+	public School get(String cd) throws Exception {
+		School school = new School();
+		
+		Connection con = getConnection();
+		
+		PreparedStatement st;
+		st = con.prepareStatement("SELECT * FROM school WHERE cd = ?");
+		st.setString(1, cd);
+		ResultSet rs = st.executeQuery();
+		
+		school.setCd(rs.getString("cd"));
+		school.setName(rs.getString("name"));
+
+		return school;
+	}
+}
