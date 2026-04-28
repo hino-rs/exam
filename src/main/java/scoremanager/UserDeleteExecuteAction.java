c2c5491b1f3b0368e93d8fa30cd87fd174897ce1
kaito hino <n041969944@gmail.com>
2026-04-28 10:18:20 +0900

    style(user)

--- src/main/java/scoremanager/UserDeleteExecuteAction.java
+++ src/main/java/scoremanager/UserDeleteExecuteAction.java
@@ -20,14 +20,12 @@ public class UserDeleteExecuteAction extends Action {
 		String loginId = loginUser.getId();
 		String deleteId = (String) request.getParameter("id");
 		
+		TeacherDao dao = new TeacherDao();
+		
 		if (loginId.equals(deleteId)) {
 			request.setAttribute("targetIsYourself", "あなた自身を消すことはできません");
 			request.getRequestDispatcher("user_delete.jsp").forward(request, response);
-		}
-		
-		TeacherDao dao = new TeacherDao();
-		
-		if (dao.delete(deleteId)) {
+		} else if (dao.delete(deleteId)) {
 			tool.Logger.info("ユーザー削除に成功");
 			request.getRequestDispatcher("user_delete_done.jsp").forward(request, response);
 		} else {
