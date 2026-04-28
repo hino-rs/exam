336a04f1c73a7a77045c3a912ceff7785c7fb072
kaito hino <n041969944@gmail.com>
2026-04-28 14:27:28 +0900

    feat: ログインしていない場合にlogin.jspに飛ばすようにした

--- src/main/java/filter/LoginFilter.java
+++ src/main/java/filter/LoginFilter.java
@@ -1,45 +1,41 @@
-//package filter;
-//
-//import java.io.IOException;
-//
-//import jakarta.servlet.Filter;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.FilterConfig;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.ServletRequest;
-//import jakarta.servlet.ServletResponse;
-//import jakarta.servlet.annotation.WebFilter;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import jakarta.servlet.http.HttpSession;
-//
-//@WebFilter(urlPatterns={"/*"})
-//public class LoginFilter implements Filter {
-//
-//	public void doFilter(
-//		ServletRequest request, ServletResponse response,
-//		FilterChain chain
-//	) throws IOException, ServletException {
-//		System.out.println("hello LoginFilter");
-//		
-//		HttpServletRequest httpReq = (HttpServletRequest) request;
-//		HttpServletResponse httpRes = (HttpServletResponse) response;
-//		
-//		String path = httpReq.getRequestURI();
-//		if (path.startsWith("/exam/LoginAction.action") || path.startsWith("/exam/css/")) {
-//			return;
-//		}
-//		
-//		HttpSession session = httpReq.getSession();
-//		
-//		Object isLogin = session.getAttribute("isLogin");
-//		
-//		if (isLogin == null) {
-//			System.out.println("to login.jsp");
-//			httpReq.getRequestDispatcher("login.jsp").forward(httpReq, httpRes);
-//		}
-//	}
-//
-//	public void init(FilterConfig filterConfig) {}
-//	public void destroy() {}
-//}
+package filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns="/*")
+public class LoginFilter implements Filter {
+	public void doFilter(
+		ServletRequest request,
+		ServletResponse response,
+		FilterChain chain
+	) throws ServletException, IOException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse res = (HttpServletResponse)response;
+		HttpSession session = req.getSession();
+		
+		String path = req.getRequestURI().substring(req.getContextPath().length());
+		tool.Logger.debug(path);
+		
+		if (path.contains("css") || path.contains("/LoginExecute") || path.contains("login.jsp")) {
+	        chain.doFilter(req, res);
+	        return;
+	    }
+		
+		if (session.getAttribute("loginUser") == null) {
+			req.getRequestDispatcher("login.jsp").forward(req, res);
+			return;
+		} else {
+			chain.doFilter(req, res);
+		}
+	}
+}
